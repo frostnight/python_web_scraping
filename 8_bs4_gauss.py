de3060d42823d7ff70cd93455b0773b4777ec119
frostnight <frostnightsong@gmail.com>
2021-10-03 02:12:47 +0900

    bs4 gauss

--- 8_bs4_gauss.py
+++ 8_bs4_gauss.py
@@ -6,13 +6,24 @@ res = requests.get(url)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "lxml")
-cartoons = soup.find_all("td", attrs={"class":"title"})
+#cartoons = soup.find_all("td", attrs={"class":"title"})
 # title = cartoons[1].a.get_text()
 # link = cartoons[0].a["href"]
 # print(title)
 # print("https://comic.naver.com" + link)
 
+# 만화 제목 + 링크 가져오기
+# for cartoon in cartoons:
+#     title = cartoon.a.get_text()
+#     link = "https://comic.naver.com" + cartoon.a["href"]
+#     print(title, link)
+
+# 평점 구하기
+cartoons = soup.find_all("div", attrs={"class", "rating_type"})
+total_rates = 0
 for cartoon in cartoons:
-    title = cartoon.a.get_text()
-    link = "https://comic.naver.com" + cartoon.a["href"]
-    print(title, link)
+    rate = cartoon.find("strong").get_text()
+    total_rates += float(rate)
+
+print("전체 점수 : ", total_rates)
+print("평균 점수 : ", total_rates / len(cartoons))

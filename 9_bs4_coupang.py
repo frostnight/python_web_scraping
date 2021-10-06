d0cbb3099ea2cbf766e515067af07d8a34562d9b
frostnight <frostnightsong@gmail.com>
2021-10-07 01:50:10 +0900

    bs4 coupang2

--- 9_bs4_coupang.py
+++ 9_bs4_coupang.py
@@ -9,6 +9,22 @@ res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
 
 items = soup.find_all("li", attrs={"class":re.compile("^search-product")})
-print(items[0].find("div", attrs={"class", "name"}).get_text())
+#print(items[0].find("div", attrs={"class", "name"}).get_text())
+
+for item in items:
+    name = items[0].find("div", attrs={"class", "name"}).get_text()
+    price = item.find("strong", attrs={"class":"price-value"}).get_text() # 가격
+    rate = item.find("em", attrs={"class":"rating"}) # 평점
+    if rate:
+        rate = rate.get_text()
+    else:
+        rate = "평점 없음"
+
+    rate_count = item.find("span", attrs={"class":"rating-total-count"}) # 평점 수
+    if rate_count:
+        rate_count = rate_count.get_text()
+    else:
+        rate_count = "평점 수 없음"
 
 
+    print(name, price, rate, rate_count)
